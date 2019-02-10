259cacb36a49ed2a3265e4b64770fb4f6db76303
Mateusz Piątkowski <mateusz.piatkowski@eestec.pl>
2019-02-10 16:47:30 +0100

    added dist to VCS! careful here

---
+++ dist/precache-manifest.222eefb05feea6914d551e4d32f9c3a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d192712258d3f8802ef",
+    "url": "/assets/css/app.ba09333f.css"
+  },
+  {
+    "revision": "4d192712258d3f8802ef",
+    "url": "/assets/js/app-legacy.ede16a1c.js"
+  },
+  {
+    "revision": "f16010003cb7a804c3c6",
+    "url": "/assets/css/chunk-vendors.0ac12124.css"
+  },
+  {
+    "revision": "f16010003cb7a804c3c6",
+    "url": "/assets/js/chunk-vendors-legacy.62becf57.js"
+  },
+  {
+    "revision": "c69cd8f5e2a5494ccf0078d9151fa950",
+    "url": "/assets/img/agh_logo.c69cd8f5.jpg"
+  },
+  {
+    "revision": "632fb083dde697b54dc9e529d23bd793",
+    "url": "/assets/img/hackathon-logo.632fb083.gif"
+  },
+  {
+    "revision": "1204fcdac678e008287c7533571696d0",
+    "url": "/assets/img/mountains-middle.1204fcda.png"
+  },
+  {
+    "revision": "85984f34e92003c6d222c8a901661172",
+    "url": "/assets/img/header-bg.85984f34.jpg"
+  },
+  {
+    "revision": "5f5321124f2d6736b91604bb76447d8b",
+    "url": "/assets/img/header-logo.5f532112.png"
+  },
+  {
+    "revision": "852e147910ac214356491f42e1b9de0c",
+    "url": "/assets/fonts/BabelStoneRunicElderFuthark.852e1479.ttf"
+  },
+  {
+    "revision": "f8f7054a555d9f69b611045315dd0c12",
+    "url": "/assets/fonts/Norse.f8f7054a.otf"
+  },
+  {
+    "revision": "a1f0ad4de31d1eb2ba0207b3dfe3a28b",
+    "url": "/assets/img/mountains-bottom.a1f0ad4d.png"
+  },
+  {
+    "revision": "16b0e94902e8563579beb7123e91653e",
+    "url": "/index.html"
+  }
+];
