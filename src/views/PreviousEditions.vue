c254c8e7c5ada7313017fa9aa079a9972b36d037
Mateusz Piątkowski <garadim12@gmail.com>
2019-10-28 21:02:03 +0100

    added router, added all views templates
    modified Footer and Header, remove unneccesary components

---
+++ src/views/PreviousEditions.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-row no-gutters>
+        previous editions
+    </b-row>
+</template>
+
+<script>
+  export default {
+    name: 'PreviousEditions',
+    data () {
+      return {}
+    },
+    components: {},
+    methods: {},
+    computed: {},
+    watch: {},
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Speakers.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-row no-gutters>
+        speakers
+    </b-row>
+</template>
+
+<script>
+  export default {
+    name: 'Speakers',
+    data () {
+      return {}
+    },
+    components: {},
+    methods: {},
+    computed: {},
+    watch: {},
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
