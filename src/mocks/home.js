87570ea6c3db86809376f2e5eaf4eb5c4c9cfca8
pawelpotaczala <paul.potaczala@gmail.com>
2019-11-09 20:29:46 +0100

    modify home

---
+++ src/mocks/home.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        "id":1,
+        "title":"Umiejętności miękkie",
+        "description":"Nasze wydarzenie skupia się wokół tematyki umiejętności miękkich, czyli wszystkich tych które potrzebne są nam do skutecznej interakcji z innymi ludźmi a także do swobodnego wyrażania siebie. Korzystamy z nich nieświadomie w każdej minucie naszego życia – na studiach, w pracy, podczas spotkań ze znajomymi czy rodziną, a także podczas nauki czy organizacji dnia. To “pierwiastek ludzki”, niezbędny w każdym procesie, niemożliwy do zastąpienia przez żadną maszynę czy program.",
+        "image":require('@/assets/images/home/um-miekkie.jpg')
+    },
+    {
+        "id":2,
+        "title":"SOFT SKILLS ACADEMY 8",
+        "year":"2017",
+        "description":"Rok 2017 był bogaty w zmiany, zmieniliśmy logo oraz całą identyfikację graficzną! Na tym nie koniec bo 8. edycja zaowocowała wspaniałymi 22 warsztatami, na których 200 naszych uczestników mogło pozyskać praktyczną wiedzę i umiejętności. Tegoroczny warsztat weekendowy został poprowadzony przez Andrzeja Kozdębę. Na wydarzeniu nie zabrakło również naszych EESTECowych trenerów, a o całym wydarzeniu można było usłyszeć m.in. w Radio 17, praca.pl, dlastudenta.pl, Magiczny Kraków i wielu innych!" +
+            "\n" +
+            "Naszymi partnerami byli: Assa Abloy, Ericsson, Nokia, Sabre, Miasto Kraków, JM prof. dr hab. Tadeusz Słomka Rektor Akademii Górniczo-Hutniczej w Krakowie.",
+        "image":require('@/assets/images/home/szkolenia.jpg')
+    },
+    {
+        "id":3,
+        "title":"SOFT SKILLS ACADEMY 7",
+        "year":"2016",
+        "description":"37 warsztatów w 7 dni - w tym 2 weekendowe warsztaty HARD - tę jakże bogatą ofertę przedstawiła w 2016 roku 7. edycja największych w Krakowie darmowych warsztatów z umiejętności miękkich przygotowana przez organizację EESTEC AGH Kraków. Zapewniono miejsca dla aż 800 uczestników, którymi byli studenci krakowskich uczelni, a o samym wydarzeniu można było usłyszeć w radio RMF Maxxx i Radio 17.\n" +
+            "\n" +
+            "Naszymi partnerami byli: firma Ericsson, firma Cisco, Miasto Kraków, JM prof. dr hab. Tadeusz Słomka Rektor Akademii Górniczo-Hutniczej w Krakowie.",
+        "image":require('@/assets/images/home/prelegenci.jpg')
+    },
+    {
+        "id":4,
+        "title":"SOFT SKILLS ACADEMY 6",
+        "year":"2015",
+        "description":"Szósta edycja Soft Skills Academy przyniosła rekordową ilość zgłoszeń - aż 1400. Chcieliśmy dać studentom coś unikatowego, więc zdecydowaliśmy się wprowadzić intensywne warsztaty weekendowe trwające 16 godzin. Ducha SSA podtrzymywali wtedy wspaniali Rafał Dąbrowski i Kamil Jarząb. Ponadto jednymi z prelegentów byli trenerzy z EESTECu, którzy na długo pozostali w pamięci uczestników.",
+        "image":require('@/assets/images/home/partnerzy.jpg')
+    }
+]
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,133 @@
 <template>
-    <b-row no-gutters>
-        <b-col cols="12" class="position-relative">
-
-        </b-col>
-    </b-row>
+    <div>
+        <b-row no-gutters class="baner position-relative align-items-center"
+               :style="{ 'background-image': `url(${require('@/assets/images/home/baner.jpg')})`}">
+            <b-col offset="1" cols="6" class="d-flex flex-column">
+                <h1 class="text-white text-uppercase">
+                    <span class="font-weight-bold display-4">10. edycja</span>
+                    <br/>
+                    darmowych warsztatów z
+                    <br/>
+                    umiejętności miękkich
+                </h1>
+                <span class="text-white">Kampus AGH, 29 listopada - 8 grudnia 2019 r.</span>
+                <b-button class="mt-2 mr-auto px-4 py-2">Zapisy od 7 listopada</b-button>
+            </b-col>
+        </b-row>
+        <b-row no-gutters class="flex-wrap-reverse flex-lg-nowrap align-items-stretch py-5">
+            <b-col offset="2" cols="8">
+                <b-row>
+                    <b-col cols="12" class="py-5 text-center text-justify">
+                        <h2>Największy w Małopolsce cykl darmowych szkoleń z zakresu umiejętności miękkich!</h2>
+                    </b-col>
+                    <b-col md="12" lg="6" class="order-1 order-lg-1 d-flex align-items-center px-4">
+                        <SVGSSAFull class="ssa-logo flex-fill"></SVGSSAFull>
+                    </b-col>
+                    <b-col md="12" lg="6" class="order-2 order-lg-2 d-flex flex-column py-3 py-lg-0">
+                        <p class="text-justify">
+                            Soft Skills Academy powraca z jubileuszową dziesiątą edycją. W tym roku zaoferujemy jeszcze
+                            więcej niż dotychczas. Na przestrzeni aż dziesięciu dni zorganizowane zostanie 40 treningów,
+                            wśród których oprócz warsztatów i wykładów znajdzie się też miejsce dla weekendowych Soft
+                            Maratonów – intensywnych całodniowych szkoleń dla najambitniejszych. Jako tegoroczną nowość
+                            wprowadzamy również pogadanki w swobodnej atmosferze.
+                        </p>
+                        <span>A wszystko to zupełnie za darmo!</span>
+                        <b-button :to="{ name: 'about' }" class="mt-2 ml-auto px-4 py-2">Dowiedz się więcej</b-button>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+        <RowContent :photo="require('@/assets/images/home/um-miekkie.jpg')">
+            <h3 class="align-self-start mb-4">Umiejętności miękkie</h3>
+            <p>
+                Nasze wydarzenie skupia się wokół tematyki umiejętności miękkich, czyli wszystkich tych które potrzebne
+                są nam do skutecznej interakcji z innymi ludźmi a także do swobodnego wyrażania siebie. Korzystamy z
+                nich nieświadomie w każdej minucie naszego życia – na studiach, w pracy, podczas spotkań ze znajomymi
+                czy rodziną, a także podczas nauki czy organizacji dnia. To “pierwiastek ludzki”, niezbędny w każdym
+                procesie, niemożliwy do zastąpienia przez żadną maszynę czy program.
+            </p>
+        </RowContent>
+        <RowContent :photo="require('@/assets/images/home/szkolenia.jpg')" reverse>
+            <h3 class="align-self-start mb-4">Szkolenia</h3>
+            <p>
+                W tym roku obchodzimy jubileusz i na dziesiątą edycję wydarzenia przypada aż dziesięć dni szkoleń,
+                podczas których poruszymy tematy takie jak:
+            </p>
+            <ul class="align-self-start">
+                <li>zarządzanie czasem,</li>
+                <li>negocjacje,</li>
+                <li>skuteczna komunikacja,</li>
+                <li>radzenie sobie ze stresem,</li>
+                <li>sztuka improwizacji,</li>
+                <li>wystąpienia publiczne,</li>
+                <li>i wiele innych…</li>
+            </ul>
+            <b-button :to="{ name: 'timetable' }" class="mt-2 mr-auto px-4 py-2">Zobacz harmonogram</b-button>
+        </RowContent>
+        <RowContent :photo="require('@/assets/images/home/prelegenci.jpg')">
+            <h3 class="align-self-start mb-4">Prelegenci</h3>
+            <p>
+                Do współpracy przy Soft Skills Academy zapraszamy ludzi, którzy umiejętności miękkie mają opanowane do
+                perfekcji i chętnie dzielą się zdobytą wiedzą. Zarówno profesjonaliści, pracujący w znanych firmach, jak
+                i niezrzeszeni mówcy, którzy po mistrzowsku opanowali zróżnicowane tematy od początku wspierali nasze
+                wydarzenie chętnie prowadząc treningi czy wykłady. Nierzadko godzina czasu tych ludzi warta jest ogromne
+                pieniądze i na ich wynajęcie mogą sobie pozwolić jedynie profesjonalne korporacje.
+            </p>
+            <b-button :to="{ name: 'speakers' }" class="mt-2 ml-auto px-4 py-2">Dowiedz się więcej o naszych prelegentach</b-button>
+        </RowContent>
+        <RowContent :photo="require('@/assets/images/home/partnerzy.jpg')" reverse>
+            <h3 class="align-self-start mb-4">Partnerzy</h3>
+            <p>
+                Od 2011 roku nasze wydarzenie odbywa się pod honorowym patronatem Jego Magnificencji Rektora Akademii
+                Górniczo-Hutniczej. Od czterech lat patronatem obejmuje nas również Miasto Kraków. Współpracujemy
+                również z wieloma firmami i organizacjami, wśród których możemy wyróżnić takie marki jak Assa Abloy czy
+                HID Global.
+            </p>
+            <b-button :to="{ name: 'partners' }" class="mt-2 mr-auto px-4 py-2">Dowiedz się więcej</b-button>
+        </RowContent>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'home',
-    data () {
-      return {}
-    },
-    components: {
+    import SVGSSAFull from '@/assets/images/ssa_full.svg'
+    import RowContent from "../components/RowContent";
 
-    },
-    methods: {},
-    computed: {},
-    watch: {},
-  }
+    export default {
+        name: 'home',
+        data() {
+            return {}
+        },
+        components: {
+            SVGSSAFull,
+            RowContent
+        },
+        methods: {},
+        computed: {},
+        watch: {},
+    }
 </script>
 
 <style scoped lang="scss">
+    .baner {
+        height: 100vh;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+
+        &:after {
+            content: "";
+            @extend .position-absolute;
+            z-index: 2;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1) 50%);
+        }
+
+        > div {
+            z-index: 3;
+        }
+    }
 
 </style>
