8551dc787a89f9e9ae8f9a46da2ceb61aba1e59e
Michał Kilian <mkilian@mielec.com.pl>
2021-11-16 07:55:51 +0100

    update partners descriptions

--- src/mocks/partners.js
+++ src/mocks/partners.js
@@ -11,36 +11,35 @@ export default [
         "type": "regular",
         "name": "ASSA ABLOY & HID",
         "description":
-            "The ASSA ABLOY Group is the global leader in access solutions. The Group is operating worldwide with 48,000 employees and sales of SEK 88 billion. ASSA ABLOY’s innovations enable safe, secure and convenient access to physical and digital places. The Group has leading positions in areas such as efficient door openings, trusted identities and entrance automation. When you walk through an automatic door, stay in a hotel or authenticate your identity, then you’re probably using our products or services. Since the beginning, ASSA ABLOY has two R&D Centers in Krakow: Group Technology Team and HID Global developing products, platforms and interfaces. In 2019 EMEA division joined with its Digital Access Solutions business." +
-            "<br/><a href='https://www.youtube.com/watch?v=FOgtWDhImRw'>ASSA ABLOY in short 2020</a>",
+            "ASSA ABLOY jest światowym liderem w rozwiązaniach dostępowych używanych codziennie na całym świecie. Przechodząc przez automatyczne drzwi, zatrzymując się w hotelu lub potwierdzając swoją tożsamość, prawdopodobnie korzystasz z naszych produktów lub usług. ASSA ABLOY & HID Global posiada w Krakowie swoje Centrum R&D gdzie korzystając z najnowszych technologii nieustannie opracowuje innowacyjne rozwiązania poprawiające jakość życia klientów.<br/><br/>ASSA ABLOY is the global leader in access solutions used every day around the world. When you walk through an automatic door, stay in a hotel or authenticate your identity, then you’re probably using our products or services. ASSA ABLOY & HID Global has its R&D Center in Krakow were using the latest technologies constantly develops innovative solutions that improve quality of life of customers.",
         "image": require('@/assets/images/partners/regular/assa-and-hid.png')
     },
     {
         "id": 3,
         "type": "regular",
         "name": "Relativity",
-        "description": "",
+        "description": "Relativity makes software to help users organize data, discover the truth and act on it. Its SaaS product RelativityOne manages large volumes of data and quickly identifies key issues during litigation and internal investigations. The AI-powered communication surveillance product, Relativity Trace proactively detects regulatory misconduct like insider trading, collusion and other non-compliant behavior. Relativity has more than 300,000 users in 49 countries serving thousands of organizations globally primarily in legal, financial services and government sectors. Please visit <a href='http://www.relativity.com'> for more information.",
         "image": require('@/assets/images/partners/regular/relativity.png')
     },
     {
         "id": 4,
         "type": "regular",
         "name": "AVSystem",
-        "description": "",
+        "description": "AVSystem jest obecne na rynku od 2006 roku i działa w trzech głównych obszarach: internetu rzeczy, telekomunikacji oraz zarządzania dostępem do WiFi. Jesteśmy liderami rynku zarządzania urządzeniami, a naszymi klientami są czołowe firmy telekomunikacyjne, przedsiębiorstwa czy sieci handlowe. Koncentrujemy się na tworzeniu skalowalnych rozwiązań, opartych o otwarte standardy, takie jak LwM2M czy TR-069. Jako pracodawcę wyróżnia nas to, że mimo globalnej obecności jesteśmy w 100% niezależną, polską firmą z siedzibą w Krakowie. Tworzymy własne oprogramowanie, bo chcemy sami zmieniać świat i kształtować rynek. Zaczynaliśmy od rozwiązań dla łączności telekomunikacyjnej, naszym kolejnym celem jest podbicie internetu rzeczy – obszaru, w którym obecnie rozwijamy się najbardziej dynamicznie.",
         "image": require('@/assets/images/partners/regular/e3adab87-ce11-43f5-a4d6-a59072da9844.png')
     },
     {
         "id": 5,
         "type": "regular",
         "name": "medius",
-        "description": "",
+        "description": "Medius is a leading provider of cloud-based spend management solutions. In particular, we help those in procurement and finance roles, but our technology truly transforms entire organizations. <br/>We’ve already made over 1300 Clients e.g. Marc Jacobs, Nissan, Hyundai, NHS, Toyota, Danone, Puma, Tikkurila and we are ready for more! <br/>Medius is a Swedish SaaS, company headquartered in Linkoping with a couple of offices in Europe, US and Australia.<br/>In Cracow office we are already 70 people working in R&D, DevOps, Customer Success, HR, IT and still growing!<br/>We are working with .Net Framework, Microsoft Azure, React, TypeScript, Azure DevOps.<br/>The most important thing to our organization is our employees – and we really mean it.",
         "image": require('@/assets/images/partners/regular/medius.png')
     },
     {
         "id": 6,
         "type": "regular",
         "name": "GE Healthcare",
-        "description": "",
+        "description": "GE Healthcare to wiodący innowator technologii medycznych, diagnostyki i rozwiązań cyfrowych. Naszą misją jest polepszanie jakości życia i ochrony zdrowia. Digital Technology Site w Krakowie to wyjątkowa przestrzeń z zespołem najwyższej klasy specjalistów IT, którzy tworzą cyfrową przyszłość dla branży medycznej na całym świecie. Nasze tętniące życiem miejsce pracy i silna kultura zorientowana na zespół są konsekwentnie nagradzane certyfikatem Great Place to Work®. Dołącz do organizacji, w której każdy głos ma znaczenie, a każdy produkt buduje zdrowszy świat.",
         "image": require('@/assets/images/partners/regular/GE-Healthcare.png')
     },
     {
