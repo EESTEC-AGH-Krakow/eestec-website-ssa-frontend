293089c32bc2f1d82e6699e8452adc4114e80681
Konrad Polarczyk <konradpolarczyk98@gmail.com>
2022-09-25 23:54:34 +0200

    Kosmetyka modali

--- src/components/PartnerModal.vue
+++ src/components/PartnerModal.vue
@@ -1,22 +1,25 @@
 <template>
-    <b-modal id="modal-partner" size="lg" centered title="Partner" title-tag="h3" ok-only>
-        <b-row no-gutters class="d-flex flex-column">
-            <b-col cols="6" class="align-self-center">
-                <img :src="currentPartner.image || ''" class="img-fluid mb-4">
-            </b-col>
-            <h3 class="mb-3">{{currentPartner.name}}</h3>
-            <article class="text-justify mt-2" v-html="currentPartner.description">
-            </article>
-        </b-row>
+    <b-modal id="modal-partner" size="lg" centered title-tag="h3" hide-footer="true">
+        <ModalContainer>
+            <b-row no-gutters class="d-flex flex-column">
+                <b-col cols="6" class="align-self-center">
+                    <img :src="currentPartner.image || ''" class="img-fluid mb-4">
+                </b-col>
+                <h3 class="mb-3 partner-name">{{currentPartner.name}}</h3>
+                <article class="text-justify mt-2 partner-description" v-html="currentPartner.description">
+                </article>
+            </b-row>
+        </ModalContainer>
     </b-modal>
 </template>
 
 <script>
     import {mapState} from 'vuex'
+    import ModalContainer from './ModalContainer.vue';
 
     export default {
         name: "PartnerModal",
-        components: {},
+        components: { ModalContainer },
         methods: {},
         watch: {},
         computed: {
@@ -26,5 +29,11 @@
 </script>
 
 <style scoped lang="scss">
-
+    .partner-name {
+        text-align: center;
+    }
+    
+    .partner-description ::v-deep a {
+        color: #7AC244 !important;
+    }
 </style>
