e4d7df10a39daee35c2eebd11f4e70c73b1a59b7
Konrad Polarczyk <konradpolarczyk98@gmail.com>
2022-09-27 22:10:42 +0200

    Poprzedni prelegenci

--- src/views/Speakers.vue
+++ src/views/Speakers.vue
@@ -1,52 +1,99 @@
 
 <template>
-    <b-row no-gutters class="d-flex justify-content-center">
-        <Speaker
-                :key="speaker.id"
-                :speaker="speaker"
-                v-for="speaker in speakers"
-        >
-        </Speaker>
-    </b-row>
+    <div class="content">
+        <b-row no-gutters class="d-flex justify-content-center">
+            <Speaker :key="speaker.id" :speaker="speaker" v-for="speaker in currentSpeakers">
+            </Speaker>
+        </b-row>
+        <b-alert v-if="this.currentSpeakers.length === 0" show variant="light" class="alert">Prelegenci Soft Skills Academy 13 pojawią się wkrótce</b-alert>
+        <b-card-header header-tag="header" class="accordion-container" role="tab">
+            <b-button block v-b-toggle.accordion-1 class="accordion-button">
+                <div class="accordion-flex"><span>Poprzednie edycje</span>
+                    <BIconChevronDown v-if=accordionOpen class="carret-icon" />
+                    <BIconChevronRight v-else class="carret-icon" />
+                </div>
+            </b-button>
+        </b-card-header>
+
+        <b-collapse id="accordion-1" :visible="accordionOpen" accordion="speaker-accordion" role="tabpanel">
+            <b-row no-gutters class="d-flex justify-content-center">
+                <Speaker :key="speaker.id" :speaker="speaker" v-for="speaker in previousSpeakers">
+                </Speaker>
+            </b-row>
+        </b-collapse>
+    </div>
 </template>
 
 <script>
     import Speaker from '../components/Speaker.vue'
-    import SPEAKERS from '../mocks/speakers.js'
+    import CurrentSpeakers from '../mocks/current-speakers.js'
+    import PreviousSpeakers from '../mocks/previous-speakers.js'
+    import { BIconChevronRight, BIconChevronDown } from 'bootstrap-vue'
 
     export default {
         name: 'Speakers',
         data() {
             return {
-                speakers: SPEAKERS
+                currentSpeakers: CurrentSpeakers,
+                previousSpeakers: PreviousSpeakers,
+                accordionOpen: CurrentSpeakers.length === 0
             }
         },
         components: {
-            Speaker
+            Speaker,
+            BIconChevronRight,
+            BIconChevronDown
         },
         methods: {},
         computed: {},
         watch: {},
+        mounted() {
+            this.$root.$on('bv::collapse::state', (collapseId, isJustShown) => {
+                if (collapseId === "accordion-1") {
+                    this.accordionOpen = isJustShown
+                }
+            })
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
-</style>
-
-<!--
-<template>
-  <p class = "temp"> <br><br>Więcej informacji wkrótce<br><br></p>
-</template>
-
-<script>
-
-</script>
-
-<style scoped lang="scss">
-.temp {
+    .alert {
+        display:flex;
+        justify-content: center;
+        align-items: center;
         text-align: center;
-        font-size: 2.7rem;
+        border-radius: 5px;
+        height: 100px;
+    }
+
+    .content {
+        margin: 30px;
+    }
+
+    .accordion-container {
+        margin-top: 50px;
+        width: 100%;
+        padding: 0;
+        background-color: transparent;
+    }
+
+    .accordion-button {
+        height: 50px;
+        color: white;
+        border-radius: 5px;
+    }
+
+    .carret-icon {
+        margin-left: 10px;
+        height: 18px;
+    }
+
+    .accordion-flex {
+        display:flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
     }
 </style>
--->
