e44d45b81804eda1bcab0aa8d97f4b06f77de001
Sławska <slawskanatalia4@gmail.com>
2022-09-29 22:58:51 +0200

    added hover effect and click event to card

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -1,6 +1,6 @@
 <template>
     <b-col cols="12" md="5" lg="4" xl="3">
-        <b-card>
+        <b-card class="grey-in-on-hover" v-b-modal.modal-speaker @click="changeSpeaker(speaker)">
             <b-card-body class="text-center text-justify">
                 <b-card-img :src="speaker.image" class="rounded-circle mb-4" v-b-modal.modal-speaker @click="changeSpeaker(speaker)" ></b-card-img>
                 <b-card-title>{{speaker.firstName}} {{speaker.lastName}}</b-card-title>
@@ -8,7 +8,7 @@
                 <b-card-text class="d-flex flex-column align-items-center">
                     <article class="text-justify">{{speaker.description | striphtml | truncate(150)}}
                     </article>
-                    <a class="justify-self-end btn-prelegent" v-b-modal.modal-speaker @click="changeSpeaker(speaker)">więcej</a>
+                    <a class="justify-self-end btn-prelegent">więcej</a>
                 </b-card-text>
             </b-card-body>
         </b-card>
@@ -35,5 +35,9 @@
         text-decoration: underline;
         color: #666666;
         margin-top: 10px;
+    }
+
+    .grey-in-on-hover :hover {
+        background-color: #ececec;
     }
 </style>
