5e3529f2a64903b1149128cb9ef81260eb99ebe4
Konrad Polarczyk <47284299+Elgirhath@users.noreply.github.com>
2022-09-30 00:14:26 +0200

    Kosmetyka modali (#23)
    * Update paczek
    
    * Remove prerender spa plugin
    
    * Fix svg not working
    
    * Add home page link to header
    
    * Dowiedz się więcej
    
    * Cleanup sass
    
    * Replace node-sass with dart-sass
    
    * Bump Travis node version
    
    * Kosmetyka modali

---
+++ src/components/ModalContainer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="modal-container">
+        <div class="modal-content">
+            <slot />
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+
+<style scoped lang="scss">
+    .modal-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-bottom: 50px;
+    }
+
+    .modal-content {
+        width: 80%;
+    }
+</style>
