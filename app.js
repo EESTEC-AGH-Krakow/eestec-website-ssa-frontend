2242325380b546f758e5906d8122451461a1f3b4
Konrad Polarczyk <konradpolarczyk98@gmail.com>
2022-10-29 21:28:08 +0200

    Fix 4

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const history = require('connect-history-api-fallback');
 
 let app = express();
-app.use(history()) //redirects calls to subpages to index.html - https://router.vuejs.org/guide/essentials/history-mode.html#native-node-js
 app.use(express.static('src'));
-app.get('/', (req, res) => {
-    res.sendFile('src/index.html');
-})
 
 const port = process.env.PORT || 5000;
 app.listen(port);
