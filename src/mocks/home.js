176a776848629bf48be2713a0e2ae923cbc4b7e4
Konrad Polarczyk <konradpolarczyk98@gmail.com>
2022-10-31 20:23:39 +0100

    Content ssa updatowany

--- src/mocks/home.js
+++ src/mocks/home.js
@@ -1,59 +1,55 @@
 export default {
   "welcome": {
     "title": "Największy w Małopolsce cykl darmowych szkoleń z zakresu umiejętności miękkich!",
-    "description": "<p class='text-justify'>"
-        + "6 grudnia odbędzie się już 12 edycja największych warsztatów"
-        + " umiejętności w Małopolsce. Podczas tej wyjątkowej edycji weźmiesz"
-        + " udział w warsztatach i wykładach, które ułatwią ci podejmowanie"
-        + " dalszych kroków w karierze jak i w życiu prywatnym. Podczas"
-        + " wydarzenia poruszone zostaną tematy z zakresu komunikacji,"
-        + " autoprezentacji czy psychologii. Jest to idealna okazja i czas do"
-        + " zainwestowania w siebie. Szkolenia są prowadzone przez"
-        + " doświadczonych trenerów, a uczestnicy warsztatów otrzymają"
-        + " certyfikat uczestnictwa."
-        + "</p>"
-        + "<span>A wszystko to zupełnie za darmo!</span>"
+    "description": "<p class='text-justify'>" + "5 grudnia rozpocznie się już 13. edycja największych warsztatów z&nbsp;"
+      + "zakresu umiejętności miękkich w&nbsp;Małopolsce. Podczas tej wyjątkowej edycji "
+      + "masz możliwość wziąć udział nie tylko w&nbsp;pojedynczych warsztatach, ale również "
+      + "w&nbsp;czymś całkowicie nowym w&nbsp;całym wydarzeniu… ścieżkach tematycznych zawierających "
+      + "kompleksowy zestaw warsztatów dobranych różnych osobowości. Podczas wydarzenia poruszone "
+      + "zostaną tematy z&nbsp;zakresu liderstwa, organizacji pracy, work-life balance czy produktywności. "
+      + "Warsztaty przeprowadzone zostaną przez doświadczonych trenerów, a&nbsp;uczestnicy otrzymają certyfikaty uczestnictwa!"
+      + "</p>"
+      + "<span>A wszystko to zupełnie za darmo!</span>"
   },
   "aboutUs": {
     "title": "Umiejętności miękkie",
     "description": "Nasze wydarzenie skupia się wokół tematyki umiejętności miękkich, czyli wszystkich"
-        + " tych które potrzebne są nam do skutecznej interakcji z innymi ludźmi a także do swobodnego"
-        + " wyrażania siebie. Korzystamy z nich nieświadomie w każdej minucie naszego życia – na studiach,"
-        + " w pracy, podczas spotkań ze znajomymi czy rodziną, a także podczas nauki czy organizacji dnia."
-        + " To “pierwiastek ludzki”, niezbędny w każdym procesie, niemożliwy do zastąpienia przez żadną maszynę czy program.",
+      + " tych które potrzebne są nam do skutecznej interakcji z innymi ludźmi a także do swobodnego"
+      + " wyrażania siebie. Korzystamy z nich nieświadomie w każdej minucie naszego życia – na studiach,"
+      + " w pracy, podczas spotkań ze znajomymi czy rodziną, a także podczas nauki czy organizacji dnia."
+      + " To “pierwiastek ludzki”, niezbędny w każdym procesie, niemożliwy do zastąpienia przez żadną maszynę czy program.",
     "image": require('@/assets/images/home/um-miekkie.jpg')
   },
   "trainings": {
     "title": "Szkolenia",
-    "description": "<p>Na dwunastą edycję wydarzenia przypada 7 dni szkoleń, podczas których poruszymy tematy takie jak:</p>"
-        + "<ul>"
-        + "<li>personal branding</li>"
-        + "<li>pewność siebie</li>"
-        + "<li>negocjacje</li>"
-        + "<li>radzenie sobie ze stresem</li>"
-        + "<li>sztuka prezentacji</li>"
-        + "<li>skuteczna komunikacja</li>"
-        + "<li>i wiele innych…</li>"
-        + "</ul>",
+    "description": "<p>Na trzynastą edycję wydarzenia trwającego 6 dni przygotowaliśmy szkolenia, których poruszymy tematy takie jak:</p>"
+      + "<ul>"
+      + "<li>komunikacja</li>"
+      + "<li>design thinking</li>"
+      + "<li>kreatywność</li>"
+      + "<li>liderstwo</li>"
+      + "<li>organizacja pracy</li>"
+      + "<li>work-life balance</li>"
+      + "<li>i wiele innych…</li>"
+      + "</ul>",
     "image": require('@/assets/images/home/szkolenia.jpg')
   },
   "speakers": {
     "title": "Prelegenci",
     "description": "Do współpracy przy Soft Skills Academy zapraszamy ludzi, którzy umiejętności miękkie mają opanowane"
-        + " do perfekcji i chętnie dzielą się zdobytą wiedzą. Zarówno profesjonaliści, pracujący"
-        + " w znanych firmach, jak i niezrzeszeni mówcy, którzy po mistrzowsku opanowali"
-        + " zróżnicowane tematy od początku wspierali nasze wydarzenie chętnie prowadząc treningi czy wykłady."
-        + " Nierzadko godzina czasu tych ludzi warta jest ogromne pieniądze i na ich wynajęcie"
-        + " mogą sobie pozwolić jedynie profesjonalne korporacje.",
+      + " do perfekcji i chętnie dzielą się zdobytą wiedzą. Zarówno profesjonaliści, pracujący"
+      + " w znanych firmach, jak i niezrzeszeni mówcy, którzy po mistrzowsku opanowali"
+      + " zróżnicowane tematy od początku wspierali nasze wydarzenie chętnie prowadząc treningi czy wykłady."
+      + " Nierzadko godzina czasu tych ludzi warta jest ogromne pieniądze i na ich wynajęcie"
+      + " mogą sobie pozwolić jedynie profesjonalne korporacje.",
     "image": require('@/assets/images/home/prelegenci.jpg')
   },
   "partners": {
     "title": "Partnerzy",
-    "description": "Od 12 lat Soft Skills Academy współpracuje z wieloma firmami"
-        + " cieszącymi się uznaniem na rynku pracy. W ramach"
-        + " wydarzenia nasi Partnerzy poprowadzą dedykowane"
-        + " warsztaty. Wśród firm, z którymi współpracujemy możemy"
-        + " wyróżnić takie marki jak&nbsp;Assa Abloy czy&nbsp;HID Global.",
+    "description": "Od 13 lat Soft Skills Academy współpracuje z wieloma firmami cieszącymi "
+      + "się uznaniem na rynku pracy. W ramach wydarzenia nasi Partnerzy poprowadzą dedykowane warsztaty. "
+      + "Wśród firm, z którymi współpracujemy możemy wyróżnić takie marki jak np. Scalaric, Relativity, GE, "
+      + "Assa Abloy czy HID Global.",
     "image": require('@/assets/images/home/partnerzy.jpg')
   }
 }
