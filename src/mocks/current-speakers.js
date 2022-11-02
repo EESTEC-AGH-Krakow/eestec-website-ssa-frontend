d26764104b8c8449633e9aaf8f820bfa573cc71f
Slawska <105443758+Slawska@users.noreply.github.com>
2022-11-02 19:26:40 +0100

    Slawska (#34)
    Dodani prelegenci

--- src/mocks/current-speakers.js
+++ src/mocks/current-speakers.js
@@ -1,2 +1,76 @@
 export default [
-]
+    {
+      id: 0,
+      firstName: 'Ania',
+      lastName: 'Kania',
+      workshopNames: [ 'Jak uczyć się szybko i efektywnie?' ],
+      description: 'Ania zajmuje się tematem efektywnej nauki od dłuższego czasu, a o sobie mówi tak: ,,Na pierwszym roku studiów z powodu stresu i nadmiaru nauki wylądowałam w szpitalu z kołataniem serca. Wtedy postanowiłam, że NIGDY nie dopuszczę do tego ponownie. Zaczęłam zagłębiać się w tajniki efektywnej nauki. Ponad 5 lat prowadzi najpopularniejsze w Polsce kursy i kanał o efektywnej nauce, a uświadamianie młodych ludzi o możliwości ich mózgów stało się jej pasją. Jest także autorką Kursu Efektywnej Nauki, dzięki któremu studenci w Polsce zdają sesję w pierwszych terminach ze świetnymi wynikami.https://aniakania.com/',
+      image: require('@/assets/images/speakers/ania-kania.png')
+    },
+    {
+      id: 1,
+      firstName: 'Wojciech',
+      lastName: 'Buda',
+      workshopNames: [
+        'Naucz się mówić “nie!” - czyli jak stać się asertywnym', 'Jak opanować stres wokół siebie, a przy tym nie zwariować?'
+      ],
+      description: 'Wojciech na co dzień jest osobą aktywnie działającą w sferze zarządzania projektami i ich sprawnej koordynacji, ale to nie wszystko - posiada rozległe doświadczenie na wielu płaszczyznach pracy z klientem, zarówno indywidualnym, jak i zagranicznym. Zajmuje się między innymi badaniem ,,Tajemniczy Klient”, nadzorowaniem projektów oraz przebiegiem szkoleń. </a><p class="text-justify">✅Jest również współautorem artykułu naukowego o stylach radzenia sobie ze stresem w wybranych sytuacjach kryzysowych: </p><a href="https://takeituseitpsychologyblog.files.wordpress.com/2021/01/style-radzenia-sobie-ze-stresem-w-wybranych-sytuacjach-kryzysowych-k.-krawcewicz-p.-polec-w.-buda.pdf">https://takeituseitpsychologyblog.files.wordpress.com/2021/01/style-radzenia-sobie-ze-stresem-w-wybranych-sytuacjach-kryzysowych-k.-krawcewicz-p.-polec-w.-buda.pdf</a><p class="text-justify">✅Prowadzi również stronę o wystąpieniach publicznych i psychologii:</p><a href="https://wojciechbuda.com.pl/">🎤 https://wojciechbuda.com.pl/</a><br><a href="https://www.linkedin.com/in/wojciech--buda/">🎤 https://www.linkedin.com/in/wojciech--buda/</a>',
+      image: require('@/assets/images/speakers/wojciech-buda.jpg')
+    },
+    {
+      id: 2,
+      firstName: 'Karolina',
+      lastName: 'Latos',
+      workshopNames: [ 'Jak gadać, żeby się dogadać?' ],
+      description: 'COO @ FreshMail, Certyfikowany Scrum Master i Facilitator. Karolina na co dzień pracuje w FreshMail, gdzie rozwija liderów oraz zespoły i kształtuje procesy w całej organizacji, bliskie są jej takie urządzenia jak Agile czy Scrum, których wartości są podstawą jej pracy. Jest również mentorką w programach mentoringowych dla liderek w branży IT i nie tylko. W pracy z ludźmi przede wszystkim jest dla niej ważna rozmowa oraz działanie, szuka ona również optymalizacji, która pozwala pracować zespołom produktywnie.',
+      image: require('@/assets/images/speakers/karolina-latos.jpeg')
+    },
+    {
+      id: 3,
+      firstName: 'Tomasz',
+      lastName: 'Waleczko',
+      workshopNames: [
+        'Zdrowe równoważenie wyzwań i obowiązków.', 'Otwartość w komunikacji.', 'Pozytywne wzmacnianie rozmówcy.'
+      ],
+      description: 'Zajmuje się między innymi rozwojem umiejętności interpersonalnych, potrzebnych w nawiązywaniu i utrzymywaniu dobrych relacji, a także rozwojem siebie. Jako psycholog pracuje z ludźmi nad układaniem życia wewnętrznego, a także uporządkowanego świata pełnego pracy, obowiązków i kompromisów. Koncentruje się głównie na budowaniu w oparciu o własne wartości.  Robi to poprzez szkolenia, coaching, treningi. Obecnie bardzo zajmuje go praca w zespole, w którym łączy możliwości AI i ludzkiego umysłu, aby dokładnie przygotowywać możliwości rozwoju potencjału każdej osoby.',
+      image: require('@/assets/images/speakers/tomasz-waleczko.jpg')
+    },
+    {
+      id: 4,
+      firstName: 'Daniel',
+      lastName: 'Stasiak',
+      workshopNames: [
+        '“Bez kija nie podchodź”, czyli jak oswoić się z przyjmowaniem i udzielaniem informacji zwrotnej'
+      ],
+      description: 'Daniel z wykształcenia jest matematykiem, a zawodowo zajmuje się promowaniem aplikacji webowych,  również jest członkiem Kraków English Public Speaking Club oraz Toastmasters. Hobbystycznie daje się porwać w świat muzyki, grając na gitarze, ewentualnie (sic!) obrywa w szachy. Pozostałe swoje zainteresowania dobiera pseudolosowo na podstawie konstelacji gwiazd danego dnia. ',
+      image: require('@/assets/images/speakers/daniel-stasiak.jpg')
+    },
+    {
+      id: 5,
+      firstName: 'Artur',
+      lastName: 'Grabowski ',
+      workshopNames: [
+        '“Bez kija nie podchodź”, czyli jak oswoić się z przyjmowaniem i udzielaniem informacji zwrotnej'
+      ],
+      description: 'Artur z wykształcenia programista, a obecnie Scrum Master w IT. Jest perfekcjonistą w temacie każdej prezentacji, prywatnie interesuje się tym, jak można poruszać się do danej muzyki, w jakim rytmie, czyli wdzięczna dyscyplina jaką jest taniec. Interesuje go również piłka nożna., ',
+      image: require('@/assets/images/speakers/artur-grabowski.jpg')
+    },
+    {
+      id: 6,
+      firstName: 'Joanna',
+      lastName: 'Bagadzińska',
+      workshopNames: [
+        'Jak cię widzą tak cię ocenią - czyli rzecz o autoprezentacji w świecie akademickim'
+      ],
+      description: 'Joanna Bagadzińska, doktorantka w Szkole Doktorskiej Uniwersytetu Warszawskiego, współpracowniczka w Ośrodku Badań nad Migracjami UW. Ukończyła z wyróżnieniem Uniwersytet Warszawski, a także UAM w Poznaniu. Jak dotąd, była prelegentką na blisko trzydziestu konferencjach naukowych, w tym czternastu międzynarodowych w kraju i za granicą. Jako Young European Ambasador (YEA) uczestniczy w wielu międzynarodowych wydarzeniach, ostatnio prezentując rekomendacje z zakresu działań na rzecz odbudowy demokracji podczas Youth Action Week organizowanego w Strasburgu przez Radę Europy. W wolnych chwilach poświęca się podróżom, gdzie poznaje nowe kultury, nowych ludzi i przede wszystkim zderza się z innymi zwyczajami w danym regionie.',
+      image: require('@/assets/images/speakers/joanna-bagadzińska.jpg')
+    },
+    {
+      id: 7,
+      firstName: 'Dawid',
+      lastName: 'Gościk',
+      workshopNames: [ 'Design thinking - jak znaleźć niszę.' ],
+      description: 'Dawid na co dzień prowadzi agencję językową oraz pracuje jako language coach - jest to praca z językiem angielskim na poziomie B2+, gdzie pokazuje on dostępne narzędzia nauki, rozwoju, czy znajdowania informacji, które są dostępne tylko w języku angielskim. Również pomaga on dyrektorom, prezesom i aktorom w ten sposób, aby rozwijać się przy pomocy technologii w wyżej wymienionym języku. Pracuje między innymi nad takimi aspektami jak zarządzanie czasem, planowanie i wyrażanie siebie oraz emocji - wszystko po angielsku!',
+      image: require('@/assets/images/speakers/dawid-goscik.jpg')
+    }
+  ]
