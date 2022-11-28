c363b1af5552f0a7208b23c9d1bde6f9af7f74e8
Konrad Polarczyk <47284299+Elgirhath@users.noreply.github.com>
2022-11-28 17:08:17 +0100

    Merge develop to master (#51)
    * Opis ssa12
    
    * Poprawki ssa (#49)

--- src/mocks/previous-editions.js
+++ src/mocks/previous-editions.js
@@ -1,20 +1,27 @@
 export default [
     {
         "id":1,
+        "title":"SOFT SKILLS ACADEMY 12",
+        "year":"2021",
+        "description":"Dwunasta edycja Soft Skills Academy odbywająca się w 2021 roku dostarczyła ponad 20 warsztatów z\xa0zakresu personal branding’u, pewności siebie, sztuki prezentacji, negocjacji i\xa0wielu innych. Jako popandemiczna edycja warsztaty i\xa0wykłady odbyły się zarówno w formie stacjonarnej jak i zdalnej ciesząc się niesamowitą obecnością i\xa0zainteresowaniem wśród uczestników dostarczając wiedzy z zakresu umiejętności miękkich. Wśród partnerów tejże edycji znaleźli się: Allegro, ASSA ABLOY, HID Global, Relativity, AVSystem, Medius, GE Healthcare, Cisco.",
+        "image":require('@/assets/images/previous-editions/ssa-12-img.jpg')
+    },
+    {
+        "id":2,
         "title":"SOFT SKILLS ACADEMY 11",
         "year":"2020",
         "description":"W 2020 roku odbyła się 11 edycja SSA, podczas której odbyło się 40 warsztatów umiejętności miękkich. Była to pierwsza w historii edycja, która w wyniku panującej pandemii zorganizowana została w całości w formie online. Pomimo trybu zdalnego edycja ta cieszyła się dużą popularnością, a ponadto osiągnęła zasięg ogólnopolski. Edycja ta została objęta Honorowym Patronatem przez Jego Magnificencję Rektora Akademii Górniczo-Hutniczej oraz Miasto Kraków.",
         "image":require('@/assets/images/previous-editions/SSA-11-img.png')
     },
     {
-        "id":2,
+        "id":3,
         "title":"SOFT SKILLS ACADEMY 10",
         "year":"2019",
         "description":"Podczas dziesiątej edycji Soft Skills Academy dostarczono szkoleń z zakresu umiejętności miękkich dla blisko 700 uczestników. Przeprowadzono 40 warsztatów, w tym 9 Soft Maratonów, czyli warsztatów całodniowych. Była to najdłuższa edycja w historii Soft Skills Academy. Wtedy też pojawił się całkiem nowy projekt towarzyszący wydarzeniu, mianowicie StartUp MeetUp, czyli panel dyskusyjny, na który zaproszeni zostali przedstawiciele krakowskich startupów. Jubileuszowa edycja została objęta Honorowym Patronatem przez Jego Magnificencję Rektora Akademii Górniczo-Hutniczej oraz Miasto Kraków. Wśród partnerów SSA10 znalazły się firmy: ASSA ABLOY, HID Global, AVSystem, Capgemini, Aon, GE Healthcare, Infolet, Motorola Solutions oraz PEGA.",
         "image":require('@/assets/images/previous-editions/ssa-10-img.jpg')
     },
     {
-        "id":3,
+        "id":4,
         "title":"SOFT SKILLS ACADEMY 9",
         "year":"2018",
         "description":"9 edycja Soft Skills Academy zaoferowała ponad 70 godzin szkoleń, które podzieliliśmy na dwa bloki tematyczne. Pierwszy z nich – Biznes – skupiał się na wykorzystaniu umiejętności miękkich w drodze na szczyt kariery, aby uczestnikom łatwiej było pracować nad projektami oraz zarządzać większą grupą ludzi. W bloku Samorozwój chcielibyśmy pokazać jak wykorzystać umiejętności miękkie w codziennym życiu. Oba bloki okazały się strzałami w dziesiątkę, gromadząc łącznie 270 uczestników.\n" +
@@ -23,7 +30,7 @@ export default [
         "image":require('@/assets/images/previous-editions/ssa-9-img.jpg')
     },
     {
-        "id":4,
+        "id":5,
         "title":"SOFT SKILLS ACADEMY 8",
         "year":"2017",
         "description":"Rok 2017 był bogaty w zmiany, zmieniliśmy logo oraz całą identyfikację graficzną! Na tym nie koniec bo 8. edycja zaowocowała wspaniałymi 22 warsztatami, na których 200 naszych uczestników mogło pozyskać praktyczną wiedzę i umiejętności. Tegoroczny warsztat weekendowy został poprowadzony przez Andrzeja Kozdębę. Na wydarzeniu nie zabrakło również naszych EESTECowych trenerów, a o całym wydarzeniu można było usłyszeć m.in. w Radio 17, praca.pl, dlastudenta.pl, Magiczny Kraków i wielu innych!" +
@@ -32,7 +39,7 @@ export default [
         "image":require('@/assets/images/previous-editions/ssa-8-img.jpg')
     },
     {
-        "id":5,
+        "id":6,
         "title":"SOFT SKILLS ACADEMY 7",
         "year":"2016",
         "description":"37 warsztatów w 7 dni - w tym 2 weekendowe warsztaty HARD - tę jakże bogatą ofertę przedstawiła w 2016 roku 7. edycja największych w Krakowie darmowych warsztatów z umiejętności miękkich przygotowana przez organizację EESTEC AGH Kraków. Zapewniono miejsca dla aż 800 uczestników, którymi byli studenci krakowskich uczelni, a o samym wydarzeniu można było usłyszeć w radio RMF Maxxx i Radio 17.\n" +
@@ -41,7 +48,7 @@ export default [
         "image":require('@/assets/images/previous-editions/ssa-7-img.jpg')
     },
     {
-        "id":6,
+        "id":7,
         "title":"SOFT SKILLS ACADEMY 6",
         "year":"2015",
         "description":"Szósta edycja Soft Skills Academy przyniosła rekordową ilość zgłoszeń - aż 1400. Chcieliśmy dać studentom coś unikatowego, więc zdecydowaliśmy się wprowadzić intensywne warsztaty weekendowe trwające 16 godzin. Ducha SSA podtrzymywali wtedy wspaniali Rafał Dąbrowski i Kamil Jarząb. Ponadto jednymi z prelegentów byli trenerzy z EESTECu, którzy na długo pozostali w pamięci uczestników.",
