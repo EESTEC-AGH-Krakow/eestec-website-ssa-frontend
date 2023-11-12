f4f0b83c0abf7a54af5f384a6839f0bc001ffbd8
patryk <patry28011@gmail.com>
2023-11-12 13:52:19 +0100

    Test modal

--- src/components/WorkshopDetails.vue
+++ src/components/WorkshopDetails.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
     <span>{{ selectedWorkshop.location }}, {{ selectedWorkshop.start }} - {{ selectedWorkshop.end }}</span>
-    <carousel class="mt-2" ref="workshop-carousel" :per-page="2" :pagination-color="'#7AC244'" autoplay loop
-      :autoplayTimeout="4000" centerMode>
-      <slide v-for="(speaker, index) in selectedWorkshop.speakers" :key="index" class="px-2 mx-auto">
-        <img :src="speaker.image" :alt="`${speaker.firstName} ${speaker.lastName}`"
-          class="img-fluid card-img rounded-circle mb-4" />
+    <carousel class="mt-2" ref="workshop-carousel" :per-page="2" :pagination-color="'#7AC244'" autoplay loop :autoplayTimeout="4000" centerMode :clickable="true">
+      <slide v-for="(speaker, index) in selectedWorkshop.speakers" :key="index" class="px-2 mx-auto" @click="openSpeakerModal(speaker)">
+        <img :src="speaker.image" :alt="`${speaker.firstName} ${speaker.lastName}`" class="img-fluid card-img rounded-circle mb-4"/>
         <h6>{{ speaker.firstName }} {{ speaker.lastName }}</h6>
       </slide>
     </carousel>
-    <!--        <b-carousel-->
-    <!--                controls-->
-    <!--                indicators-->
-    <!--        >-->
-    <!--            <b-carousel-slide v-for="(speaker, index) in selectedWorkshop.speakers" :key="index"-->
-    <!--                              :caption="`${speaker.firstName} ${speaker.lastName}`"-->
-    <!--                              :img-src="speaker.image"></b-carousel-slide>-->
-    <!--        </b-carousel>-->
     <article v-html="selectedWorkshop.description" class="text-justify"></article>
     <speaker-modal ref="speakerModal"></speaker-modal>
   </div>
@@ -24,15 +14,16 @@
 
 <script>
 import SpeakerModal from './SpeakerModal.vue';
+
 export default {
   name: 'WorkshopDetails',
   data() {
-    return {}
+    return {};
   },
   mounted() {
     setTimeout(() => {
-      this.$forceUpdate()
-    }, 0)
+      this.$forceUpdate();
+    }, 0);
   },
   props: ['selectedWorkshop'],
   components: {
@@ -43,9 +34,17 @@ export default {
       this.$refs.speakerModal.open(speaker);
     },
   },
-  computed: {},
-  watch: {},
-}
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
+    <!--        <b-carousel-->
+    <!--                controls-->
+    <!--                indicators-->
+    <!--        >-->
+    <!--            <b-carousel-slide v-for="(speaker, index) in selectedWorkshop.speakers" :key="index"-->
+    <!--                              :caption="`${speaker.firstName} ${speaker.lastName}`"-->
+    <!--                              :img-src="speaker.image"></b-carousel-slide>-->
+    <!--        </b-carousel>-->
